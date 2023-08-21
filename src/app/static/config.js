a89d60cbfbe3b5117488af0f370139cba6f1273f
DaniAffCH <danieleaffinita2000@gmail.com>
2023-08-21 16:21:30 +0200

    path fix

--- src/app/static/config.js
+++ src/app/static/config.js
@@ -18,15 +18,15 @@ export const config = {
 };
 
 export const modelMapping = new Map([
-[Generator, ['../../assets/models/generator/sci-fi_cargo_crate.glb',1]],
-[MainRobot, ['../../assets/models/hmo-man/hmo-ng.glb',1]],
-[Platform ,['../../assets/models/platform/scifi_platform.glb',1]],
-[Door , ['../../assets/models/door/space_door.glb',1]],
-[Pillar, ['../../assets/models/pillar/pillar.glb',6]],
-[Button, ['../../assets/models/button/scifi_button.glb',3]],
-[Desk, ['../../assets/models/desk/scifi_desk.glb',1]],
-[Book, ['../../assets/models/book/book_open.glb',1]],
-[Trophy, ['../../assets/models/trophy/world_cup_trophy.glb',1]],
-[Redcarpet, ['../../assets/models/redcarpet/red_carpet.glb',1]],
-[Rope, ['../../assets/models/rope/red_velvet_rope.glb',16]]
+[Generator, ['assets/models/generator/sci-fi_cargo_crate.glb',1]],
+[MainRobot, ['assets/models/hmo-man/hmo-ng.glb',1]],
+[Platform ,['assets/models/platform/scifi_platform.glb',1]],
+[Door , ['assets/models/door/space_door.glb',1]],
+[Pillar, ['assets/models/pillar/pillar.glb',6]],
+[Button, ['assets/models/button/scifi_button.glb',3]],
+[Desk, ['assets/models/desk/scifi_desk.glb',1]],
+[Book, ['assets/models/book/book_open.glb',1]],
+[Trophy, ['assets/models/trophy/world_cup_trophy.glb',1]],
+[Redcarpet, ['assets/models/redcarpet/red_carpet.glb',1]],
+[Rope, ['assets/models/rope/red_velvet_rope.glb',16]]
 ]);
